a4b0303fcf7e6dda5e29227d1c32d58bee3da87d
excelwizpro <jsanghe123@gmail.com>
2025-12-07 13:03:26 +0000

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src")
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-  server: {
-    port: 5173
-  },
+
   build: {
-    outDir: "dist"
-  }
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+
+  base: "/", // IMPORTANT FOR VERCEL
 });
